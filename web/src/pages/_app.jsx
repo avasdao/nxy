c0ad37f887cd1bc8d4f813f0a9d3ebdffaae42e5
Ava's DAO <support@avasdao.org>
2024-02-09 20:06:04 -0500

    Add analytics.

--- web/src/pages/_app.jsx
+++ web/src/pages/_app.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import { init as matomo } from '@socialgouv/matomo-next'
+
+/* Initialize Matomo parameters. */
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || '//matomo.avasdao.org'
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || 22
 
 export default function App({ Component, pageProps }) {
+    /* Handle (Matomo) analytics. */
+    useEffect(() => {
+        matomo({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
+    }, [])
+
     return <Component {...pageProps} />
 }
