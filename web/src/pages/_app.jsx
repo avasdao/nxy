b05f417d82508e2760827d0bbe169647f89480c4
Ava's DAO <support@avasdao.org>
2024-02-09 20:17:58 -0500

    Update analytics.

--- web/src/pages/_app.jsx
+++ web/src/pages/_app.jsx
@@ -5,7 +5,7 @@ import { init as matomo } from '@socialgouv/matomo-next'
 
 /* Initialize Matomo parameters. */
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || '//matomo.avasdao.org'
-const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || 22
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || 2
 
 export default function App({ Component, pageProps }) {
     /* Handle (Matomo) analytics. */
