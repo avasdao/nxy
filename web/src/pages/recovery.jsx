e74d9a6f00c29164c614c025a86551ece1c2c2d2
Ava's DAO <support@avasdao.org>
2024-10-17 01:59:18 -0400

    Add social recovery page.

---
+++ web/src/pages/recovery.jsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { AuthLayout } from '@/components/AuthLayout'
+import { Button } from '@/components/Button'
+import { TextField } from '@/components/Fields'
+
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import { BookmarkSquareIcon, BookOpenIcon, QueueListIcon, RssIcon } from '@heroicons/react/24/solid'
+
+import logo from '@/images/logo.png'
+
+export default function Login() {
+    // TODO
+
+    return (
+        <>
+            <Head>
+                <title>Recovery</title>
+            </Head>
+
+            <div className="bg-white">
+                <main className="mx-auto w-full max-w-7xl px-6 pb-16 pt-10 sm:pb-24 lg:px-8">
+                    <Image
+                        className="mx-auto h-10 w-auto sm:h-12"
+                        src={logo}
+                        alt="Nxy Social logo"
+                        width="64px"
+                        height="64px"
+                    />
+
+                    <div className="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
+                        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                            Welcome to <span className="text-rose-500 text-7xl italic font-normal">Social Recovery</span> for Owners
+                        </h1>
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus quidem nisi quia eaque, inventore vel cum nesciunt voluptatem voluptates beatae! Atque possimus accusamus minima consectetur repellendus delectus qui exercitationem labore!
+                    </p>
+
+                </main>
+
+            </div>
+        </>
+    )
+}
