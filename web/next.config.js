9cfcc9436ed9b182e9c91c435f58f9c8e3ac41d4
Ava's DAO <support@avasdao.org>
2024-10-21 08:54:33 -0400

    Fix (auto) redirect.

--- web/next.config.js
+++ web/next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
       allowFutureImage: true,
     },
   },
-  async redirects() {
-    return [
-      {
-        source: '/L1',
-        destination: '/l1',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
